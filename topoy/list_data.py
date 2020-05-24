8f2c96c4c8861061cdd09794a9f4d4333f3bfb49
beezee <brian.zeligson@gmail.com>
2020-05-24 08:56:22 -0400

    data -> infix -> instances

--- topoy/list_data.py
+++ topoy/list_data.py
@@ -16,50 +16,44 @@ class ListData(HKT[ListF, A]):
   def __init__(self, run: PyList[A]) -> None:
     self.run = run
 
-  @staticmethod
-  def _inj(l: 'ListData[A]') -> 'HKT[ListF, A]':
-    return cast(HKT[ListF, A], l)
-
-  @staticmethod
-  def _proj(hkt: 'HKT[ListF, A]') -> 'ListData[A]':
-    return cast('ListData[A]', hkt)
+import topoy.list as l
 
 class ListFunctor(Functor[ListF]):
 
   def map(self, fa: HKT[ListF, A], f: Callable[[A], B]) -> HKT[ListF, B]:
-    return ListData([f(a) for a in ListData._proj(fa).run])
+    return l.List([f(a) for a in l.List.proj(fa).run])
 
 class ListMonad(ListFunctor, Monad[ListF]):
 
   def point(self, a: A) -> HKT[ListF, A]:
-    return ListData([a])
+    return l.List([a])
 
   def bind(self, fa: HKT[ListF, A],
            f: Callable[[A], HKT[ListF, B]]) -> HKT[ListF, B]:
-    return ListData(
-      [x for y in ListData._proj(fa).run
-         for x in ListData._proj(f(y)).run])
+    return l.List(
+      [x for y in l.List.proj(fa).run
+         for x in l.List.proj(f(y)).run])
 
 class ListApply(Apply[ListF], ListFunctor):
 
   def ap(self, fa: HKT[ListF, A],
        fab: HKT[ListF, Callable[[A], B]]) -> HKT[ListF, B]:
-    return ListData(
-      [f(a) for a in ListData._proj(fa).run
-            for f in ListData._proj(fab).run])
+    return l.List(
+      [f(a) for a in l.List.proj(fa).run
+            for f in l.List.proj(fab).run])
 
 class ListApplicative(Applicative[ListF], ListApply):
 
   def pure(self, a: A) -> HKT[ListF, A]:
-    return ListData([a])
+    return l.List([a])
 
 class ListPlusEmpty(PlusEmpty[ListF]):
 
   def empty(self, t: Type[A]) -> HKT[ListF, A]:
-    return ListData[A]([])
+    return l.List[A]([])
 
   def plus(self, la1: HKT[ListF, A], la2: HKT[ListF, A]) -> HKT[ListF, A]:
-    return ListData(ListData._proj(la1).run + ListData._proj(la2).run)
+    return l.List(l.List.proj(la1).run + l.List.proj(la2).run)
 
 class ListTraverse(Traverse[ListF], ListFunctor):
 
@@ -74,5 +68,5 @@ class ListTraverse(Traverse[ListF], ListFunctor):
           return x
         return ap.ap(f(e), ap.map(acc, m))
       z: HKT[G, PyList[B]] = ap.pure([])
-      return ap.map(reduce(reducer, ListData._proj(fa).run, z), ListData)
+      return ap.map(reduce(reducer, l.List.proj(fa).run, z), l.List)
 
